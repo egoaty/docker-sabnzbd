f737e2c03d472636d4c5698f71459390641df522
Roland Gadinger <github@e-goaty.com>
2022-03-14 17:26:41 +0100

    bugfixed run.sh for Ubuntu

--- root/run.sh
+++ root/run.sh
@@ -10,13 +10,13 @@
 user="dlmanager"
 group="dlmanager"
 if ! id ${user} >/dev/null 2>&1; then 
-	addgroup -g "${PGID:=100000}" "${group}"
-	adduser -h / -H -D -G "${group}" -u "${PUID:=100000}" "${user}"
+	groupadd -g "${PGID:=100000}" "${group}"
+	useradd -d / -M -g "${group}" -u "${PUID:=100000}" "${user}"
 fi
 
 chown -R ${user}:${group} /config
 
 # Run SABnzbd in foreground
 exec su - ${user} -c \
-	'/opt/sabnzbd/SABnzbd.py --config-file /config --server 0.0.0.0:8080'
+	'sabnzbdplus --config-file /config --server 0.0.0.0:8080'
 
